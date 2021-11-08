a963283e91c20dd3bcedc88aedd563cfb1b381f7
Zaiming Shi <zmstone@gmail.com>
2021-11-08 22:13:25 +0100

    test: add a test cate to ensure consitent vsn in .app and .appup

---
+++ test/ehttpc_appup_tests.erl
@@ -0,0 +1,25 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2021 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(ehttpc_appup_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+app_vsn_test() ->
+    {ok, [{AppupVsn, _Upgraes, _Downgrades}]} = file:consult("src/ehttpc.appup.src"),
+    {ok, [{application, ehttpc, Props}]} = file:consult("src/ehttpc.app.src"),
+    Vsn = proplists:get_value(vsn, Props),
+    ?assertEqual(Vsn, AppupVsn).
